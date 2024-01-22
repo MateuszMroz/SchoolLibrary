d92596fb8071dfa27a6534c2b7787ea2367d2509
Mateusz Mroz <mateusz.mroz@miquido.com>
2024-01-22 09:43:39 +0100

    init commit

---
+++ src/main/kotlin/com/mrozm/schoollibrary/Application.kt
@@ -0,0 +1,11 @@
+package com.mrozm.schoollibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchoolLibraryApplication>(*args)
+}
